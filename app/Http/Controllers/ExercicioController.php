1329d2aaeb6643dac2743675fdf4d7dc90104344
Gabriel <gabrantesrodrigues@gmail.com>
2026-02-19 20:56:10 -0300

    Inicializando

---
+++ app/Http/Controllers/ExercicioController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ExercicioController extends Controller {
+
+    public function exibiFormulario(){
+        return view('exercicio');
+    }
+
+    public function calcularSoma(Request $request){
+        $valor1 = $request->input('valor1');
+        $valor2 = $request->input('valor2');
+        $soma = $valor1 + $valor2;
+        return view('exercicio',['soma' => $soma]);
+    }
+
+    public function exibirFormulario(Request $request){
+        $valor2 = $resquest->input('valor2');
+        $valor2 = $resquest->input('valor2');
+        $sub = $valor2 - $valor2;
+        return view('exercicio2',['sub'=> $sub]);
+}
